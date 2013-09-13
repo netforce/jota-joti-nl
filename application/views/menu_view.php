95b548e2f87f030dc5d84eb397327c336f86332b
netforce <netforce@scoutlink.net>
2013-09-13 13:56:37 +0200

    - Bijlagen management is onder Onderdelen

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -79,7 +79,6 @@
                                 <li class="divider"></li>
                                 <li><a tabindex="-1" href="<?php echo base_url();?>beheer/spel">Spelen</a></li>
                                 <li><a tabindex="-1" href="<?php echo base_url();?>beheer">Onderdelen</a></li>
-                                <li><a tabindex="-1" href="<?php echo base_url();?>beheer/bijlagen">Bijlagen</a></li>
                             <?php } ?>
 
                             <?php if ($this->session->userdata('pagina')) { ?>
